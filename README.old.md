bdc76f3058bab25dc98d5525b3aba3401b5141d8
Krisztina Andreyka <andreyka.kriszti@gmail.com>
2020-02-28 21:05:40 +0100

    Updates

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# ppc
-Plate process counter
---
+++ precache-manifest.17f1627c263c363f2a117bdcae5562f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0720a68eadc2848b14a38bae2349c17",
+    "url": "/ppc/index.html"
+  },
+  {
+    "revision": "d846a6ddd6af487093c4",
+    "url": "/ppc/static/css/main.73112ca5.chunk.css"
+  },
+  {
+    "revision": "e65966c526c3abcb36e8",
+    "url": "/ppc/static/js/2.25184b41.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/ppc/static/js/2.25184b41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d846a6ddd6af487093c4",
+    "url": "/ppc/static/js/main.682aaacd.chunk.js"
+  },
+  {
+    "revision": "8091b97561a83d883df0",
+    "url": "/ppc/static/js/runtime-main.600c90b1.js"
+  }
+]);
